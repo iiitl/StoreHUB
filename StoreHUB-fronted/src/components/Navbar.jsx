37191edef53326c54c12ad54891bd85d1bc94f2a
Rohangarg <garg20005rohan@gmail.com>
2025-03-28 16:46:19 +0530

    Added dynamic links that shows where the user is

--- StoreHUB-fronted/src/components/Navbar.jsx
+++ StoreHUB-fronted/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Home, Flame, Book, Users, UserCircle2, Menu, X } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
@@ -14,15 +14,20 @@ const Navbar = () => {
     }
   }, [user, navigate]);
 
+  //Function to return active , inactive classes for NavLinks
+  const getLinkClasses = (base = "") => ({ isActive }) =>
+    `${base} ${isActive ? "text-black font-bold underline" : "text-black/70"} transition-colors`;
+
+
   return (
     user && (
       <nav className="fixed top-0 left-0 right-0 bg-white border-b border-black/10 z-50">
         <div className="container mx-auto flex justify-between items-center p-4">
       
           <div className="flex items-center justify-between w-full lg:w-auto">
-            <Link to="/">
+            <NavLink to="/">
               <h1 className="text-2xl font-bold text-black">StoreHUB</h1>
-            </Link>
+            </NavLink>
             <button
               className="lg:hidden block text-black/70"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -33,84 +38,84 @@ const Navbar = () => {
 
        
           <div className="hidden lg:flex items-center space-x-8 text-black">
-            <Link to="/">
+            <NavLink to="/">
               <NavItem icon={<Home />} label="Explore" />
-            </Link>
-            <Link to="/trending">
+            </NavLink>
+            <NavLink to="/trending">
               <NavItem icon={<Flame />} label="Trending" />
-            </Link>
-            <Link to="/sandbox">
+            </NavLink>
+            <NavLink to="/sandbox">
               <NavItem icon={<Book />} label="Sandbox" />
-            </Link>
-            <Link to="/discussion">
+            </NavLink>
+            <NavLink to="/discussion">
               <NavItem icon={<Users />} label="Discussion" />
-            </Link>
+            </NavLink>
           </div>
 
         
           <div className="hidden lg:flex items-center space-x-4">
-            <Link to={"/create"}>
+            <NavLink to={"/create"}>
               <button className="text-black border-black/70 border px-4 py-2 rounded-lg flex items-center">
                 Create
               </button>
-            </Link>
-            <Link to="/profile">
+            </NavLink>
+            <NavLink to="/profile">
               <div className="flex items-center space-x-2">
                 <UserCircle2 className="text-black/70" size={40} />
                 <span className="font-medium">
                   {user.user?.username || "John"}
                 </span>
               </div>
-            </Link>
+            </NavLink>
           </div>
         </div>
 
   
         {isMenuOpen && (
           <div className="lg:hidden flex flex-col bg-white border-t border-black/10">
-            <Link
+            <NavLink
               to="/"
               className="p-4 text-black hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <NavItem icon={<Home />} label="Explore" />
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/trending"
               className="p-4 text-black hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <NavItem icon={<Flame />} label="Trending" />
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/sandbox"
               className="p-4 text-black hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <NavItem icon={<Book />} label="Sandbox" />
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/discussion"
               className="p-4 text-black hover:bg-gray-100"
               onClick={() => setIsMenuOpen(false)}
             >
               <NavItem icon={<Users />} label="Discussion" />
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/create"
               className="p-4 text-black hover:bg-gray-100 md:border md:border-1 md:border-black/10" 
               onClick={() => setIsMenuOpen(false)}
             >
               Create
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/profile"
               className="p-4 text-black hover:bg-gray-100 flex items-center space-x-2"
               onClick={() => setIsMenuOpen(false)}
             >
               <UserCircle2 className="text-black/70" size={30} />
               <span>{user.user?.username || "John"}</span>
-            </Link>
+            </NavLink>
           </div>
         )}
       </nav>
